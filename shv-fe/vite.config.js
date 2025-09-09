a8d5f54c377fe6aa3bef98612bb9f7a3b21bfdca
shophuyvan <kinhdoanhonlinegiasoc@gmail.com>
2025-09-09 17:44:49 +0700

    Update vite.config.js

--- shv-fe/vite.config.js
+++ shv-fe/vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
   build: {
+    target: 'esnext',
     outDir: 'dist',
-  },
+    rollupOptions: {
+      input: {
+        index:    resolve(__dirname, 'index.html'),
+        product:  resolve(__dirname, 'product.html'),
+        cart:     resolve(__dirname, 'cart.html'),
+        checkout: resolve(__dirname, 'checkout.html'),
+        admin:    resolve(__dirname, 'admin.html'),
+        uploader: resolve(__dirname, 'uploader.html')
+      }
+    }
+  }
 });
