9bebca58a870618226d3b939baa40037a198b323
shophuyvan <kinhdoanhonlinegiasoc@gmail.com>
2025-09-20 15:06:34 +0700

    Delete shv-api directory

--- shv-api/src/modules/orders.js
+++
@@ -1,7 +0,0 @@
-/* SHV safe patch header */
-export async function handleOrders(req, env, fire) {
-  const body = await req.json();
-  // TODO: write to Firestore and update users revenue
-  const orderId = 'ORD' + Date.now();
-  return new Response(JSON.stringify({ orderId }), { headers: { 'content-type':'application/json' }});
-}
