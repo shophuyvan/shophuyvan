980be313caf134782eba9408a8247bef2a41f2c9
shophuyvan <kinhdoanhonlinegiasoc@gmail.com>
2025-09-20 11:21:32 +0700

    Delete shophuyvan-main directory

--- shophuyvan-main/shv-api/src/modules/shipping/providers/jt.js
+++
@@ -1,6 +0,0 @@
-/* SHV safe patch header */
-export const jtAdapter = {
-  async quote(ctx){ return []; },
-  async create(ctx){ return { tracking_code:'', fee:0, eta:'' }; },
-  async cancel(ctx){ return { ok:true }; }
-};
