9c1e642fb46de8d8f051af071445849088eee8d6
shophuyvan <kinhdoanhonlinegiasoc@gmail.com>
2025-11-02 17:57:46 +0700

    fix order ver 17

--- apps/mini/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import zmp from "zmp-vite-plugin";
-
-export default defineConfig({
-  plugins: [react(), zmp()],
-  base: "./"
-});
